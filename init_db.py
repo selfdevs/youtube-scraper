bfc928e5a3a1bfbe6a420b4e527b333f9d6e37f7
ArthThapa <arththapa7@gmail.com>
2022-07-17 12:15:54 +0530

    Implement multiprocessing solves #3

--- init_db.py
+++ init_db.py
@@ -3,13 +3,26 @@ import sqlite3
 import traceback
 
 
-def create_connection(local_db_path):
-    try:
-        conn = sqlite3.connect(local_db_path)
-        # logging.info(f"SQLite DB has been created with version {sqlite3.version}")
-        return conn
-    except Error as e:
-        print(f"Error occured while connecting to DB: {e}")
+def create_connection(local_db_path, table_name):
+
+    conn = sqlite3.connect(local_db_path, check_same_thread=False)
+    # # logging.info(f"SQLite DB has been created with version {sqlite3.version}")
+
+    # # mycursor = conn.cursor()
+    # # tables = mycursor.execute("TABLES")
+    # # print(type(tables))
+    # cursor = conn.cursor()
+    # # try:
+    # #     cursor.execute(
+    # #         f'SELECT * FROM SQLITE_SCHEMA WHERE type="table" AND TABLE_NAME="{table_name}"'
+    # #     ).fetchall()
+    # # except sqlite3.OperationalError as e:
+    # cursor.execute(
+    #     f"CREATE TABLE IF NOT EXISTS {table_name} (video_id VARCHAR(255) PRIMARY KEY, view_count BIGINT(255), title VARCHAR(255), pub_time VARCHAR(255), duration VARCHAR(255), channel_name VARCHAR(255), channel_id VARCHAR(255), channel_link VARCHAR(255), thumbnails VARCHAR(255))"
+    # )
+
+    # print(f"Connected to {table_name}")
+    return conn
 
 
 def create_db(conn, config_data):
--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from utils.load_config import (
 )
 import pandas as pd
 import init_db as db_helper
+from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
 
+db_conn = db_helper.create_connection("2.db", table_name)
 dict_for_df = {
     "view_count": [],
     "title": [],
@@ -22,63 +25,65 @@ dict_for_df = {
     "channel_link": [],
     "thumbnails": [],
 }
-
-json_file = open("test.json", "w")
+searched_keywords = set()
+suggestions = Suggestions(region=suggestions_region, language=suggestions_language)
 
 
 def get_results(kw):
+    global kw_list
     search = VideosSearch(kw)
-    try:
-        print(kw)
-        for page in range(0, page_per_kw):
-            for i in range(0, len(search.result()["result"])):
-                data = search.result()["result"][i]
-                if data["viewCount"]["text"] and "views" in data["viewCount"]["text"]:
-                    if (
-                        data["viewCount"]["text"] != "No views"
-                        or data["viewCount"]["text"] != ""
-                        or data["viewCount"]["text"] is not None
-                    ):
-                        views = int(re.sub("[^0-9]", "", data["viewCount"]["text"]))
-                        if views >= views_bar:
-                            dict_for_df["view_count"].append(views)
-                            dict_for_df["title"].append(data["title"])
-                            dict_for_df["channel_id"].append(data["channel"]["id"])
-                            dict_for_df["channel_link"].append(data["channel"]["link"])
-                            dict_for_df["channel_name"].append(data["channel"]["name"])
 
-                            dict_for_df["duration"].append(data["duration"])
-                            dict_for_df["pub_time"].append(data["publishedTime"])
-                            dict_for_df["thumbnails"].append(
-                                data["thumbnails"][0]["url"]
-                            )
-                            dict_for_df["video_id"].append(data["id"])
-    except Exception as e:
-        print(views)
+    print(f"keywords: {keywords}")
+    print(kw)
+    for page in range(0, page_per_kw):
+        for i in range(0, len(search.result()["result"])):
+            data = search.result()["result"][i]
+            if data["viewCount"]["text"] and "views" in data["viewCount"]["text"]:
+                if (
+                    data["viewCount"]["text"] != "No views"
+                    or data["viewCount"]["text"] != ""
+                    or data["viewCount"]["text"] is not None
+                ):
+                    views = int(re.sub("[^0-9]", "", data["viewCount"]["text"]))
+                    if views >= views_bar:
+                        dict_for_df["view_count"].append(views)
+                        dict_for_df["title"].append(data["title"])
+                        dict_for_df["channel_id"].append(data["channel"]["id"])
+                        dict_for_df["channel_link"].append(data["channel"]["link"])
+                        dict_for_df["channel_name"].append(data["channel"]["name"])
+
+                        dict_for_df["duration"].append(data["duration"])
+                        dict_for_df["pub_time"].append(data["publishedTime"])
+                        dict_for_df["thumbnails"].append(data["thumbnails"][0]["url"])
+                        dict_for_df["video_id"].append(data["id"])
 
         data = search.next()
 
+    pd.DataFrame.from_dict(dict_for_df).to_sql(
+        name=table_name,
+        if_exists="append",
+        con=db_conn,
+        index=False,
+    )
+    searched_keywords.add(kw)
+    keywords.pop(0)
+    suggested = set(suggestions.get(kw)["result"])
+    for suggested in suggested:
+        if suggested not in searched_keywords:
+            print(f"suggested from {kw}: {suggested}")
+            keywords.append(suggested)
+    pool = ThreadPool(6)
+    pool.map(get_results, keywords)
+    pool.close()
+    pool.join()
 
-def main():
-    db_conn = db_helper.create_connection("yt.db")
-    searched_keywords = set()
-    suggestions = Suggestions(region=suggestions_region, language=suggestions_language)
-    print(keywords)
+
+if __name__ == "__main__":
     try:
-        for keyword in keywords:
-            if keyword not in searched_keywords:
-                get_results(keyword)
-                pd.DataFrame.from_dict(dict_for_df).to_sql(
-                    name={table_name}, if_exists="append", con=db_conn, index=False
-                )
-                searched_keywords.add(keyword)
-                suggested = set(suggestions.get(keyword)["result"])
-                for kw in suggested:
-                    if kw not in searched_keywords:
-                        keywords.append(kw)
+        print(table_name)
+        pool = Pool(6)
+        pool.map(get_results, keywords)
+        pool.close()
+        pool.join()
     except Exception as e:
-        print(f"keyword: {keyword}")
-        print(e)
-
-
-main()
+        print(f"Exception occured while executing main {e}")
